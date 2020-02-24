352a0d4cdee9eac583e61b2416340a6ae3311afb
kaygb <48642860+kaygb@users.noreply.github.com>
2020-02-24 17:00:23 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "风知识"
+site_name = "疯知识"
 # site_logo = "${static_prefix}logo.png"
 site_logo = "https://cdn.v2ex.com/gravatar/4cc893d113dd74ceca73f9863f2c5446/"
 site_build_date = "2020-02-24T13:00+08:00"
@@ -23,21 +23,36 @@ author = "风也温柔"
 email = "kaygbtop@163.com"
 author_homepage = "https://eas1.cn"
 description = "风也温柔，但你更美！"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+key_words = ['疯知识', '风也温柔', 'wiki', 'blog']
 language = 'zh-CN'
 external_links = [
     {
-        "name": "风也温柔",
+        "name": "BLOG",
         "url": "https://kaygb.top",
         "brief": "风也温柔的主页。"
+    },
+    {
+        "name": "BAIDU",
+        "url": "https://baidu.kaygb.top",
+        "brief": "帮你百度一下。"
+    },
+    {
+        "name": "KTOOLS",
+        "url": "https://tools.kaygb.top",
+        "brief": "在线工具箱。"
+    },
+    {
+        "name": "STATUS",
+        "url": "https://status.kaygb.top",
+        "brief": "风也温柔的服务监控。"
     }
 ]
 nav = [
-    {
-        "name": "首页",
-        "url": "${site_prefix}",
-        "target": "_self"
-    },
+#     {
+#         "name": "首页",
+#         "url": "${site_prefix}",
+#         "target": "_self"
+#     },
     {
         "name": "归档",
         "url": "${site_prefix}archives/",
