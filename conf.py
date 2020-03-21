7c4ccf086352e70c01f8c4cb4164bf893fbb9129
kaygb <48642860+kaygb@users.noreply.github.com>
2020-03-21 17:54:16 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -92,7 +92,6 @@ head_addon = r'''
 <meta http-equiv="x-dns-prefetch-control" content="on">
 <link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
 <link rel="icon" type="image/ico" href="//gravatar.kaygb.top/avatar/4cc893d113dd74ceca73f9863f2c5446/">
-<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.18.1/build/styles/default.min.css">
 
 '''
 
@@ -103,6 +102,6 @@ footer_addon = r'''
 '''
 
 body_addon = r'''
-<script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.18.1/build/highlight.min.js"></script>
+
 '''
 
