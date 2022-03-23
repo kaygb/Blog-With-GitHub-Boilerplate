4e710fff9854603f219b7d1f202199e40b223598
kaygb <kaygb@qq.com>
2022-03-23 19:50:21 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@
 """
 
 # For Maverick
-site_prefix = "https://kaygb.github.io/wikisource/"
+site_prefix = "https://wiki.170601.xyz/"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
@@ -26,7 +26,7 @@ site_logo = "https://cdn.jsdelivr.net/gh/kaygb/twentytwenty/tx.jpg"
 site_build_date = "2020-02-24T13:00+08:00"
 author = "珂泽"
 email = "hi@vwmwv.cn"
-author_homepage = "https://www.vwmwv.cn"
+author_homepage = "https://170601.xyz"
 description = "珂泽的Wiki站点，用来记录零零散散的笔记！"
 key_words = ['珂泽笔记','疯知识','风也的小站' ,'风也温柔', 'wiki', 'blog']
 language = 'zh-CN'
@@ -34,7 +34,7 @@ external_links = [
     
     {
         "name": "HOME",
-        "url": "https://www.vwmwv.cn/",
+        "url": "https://170601.xyz",
         "brief": "HOME"
     },
     {
@@ -89,7 +89,7 @@ head_addon = r'''
 '''
 
 footer_addon = r'''
-<a href="http://beian.miit.gov.cn/" target="_blank" rel="noopener">豫ICP备19002054号-8</a>
+<a href="http://beian.miit.gov.cn/" target="_blank" rel="noopener">豫ICP备19002054号-5</a>
 '''
 
 body_addon = r'''
